481a12c5cf27d15ef706afb035ca52ff828b568b
Heremenegildo Wilson <hermenegildowilson7@gmail.com>
2025-11-16 11:25:44 +0100

    Restruturação do formularios

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -74,7 +74,7 @@ export function Input({
 
 export function FormPage({ children }) {
     return (
-        <div className="min-h-[80vh] flex flex-col justify-center">
+        <div className="min-h-[70vh] flex flex-col justify-center">
             {children}
         </div>
     );
